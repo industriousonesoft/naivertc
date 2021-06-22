86c67d20ffef084773ebc83bd6ccc776f70abd18
industriousonesoft <industriousonesoft@gmail.com>
2021-06-22 12:04:10 +0800

    Integrated GTest

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -34,7 +34,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/markcao/Documents/Github_Codes/NaiveRTC/build/deps/plog/cmake_install.cmake")
 
 endif()
 
