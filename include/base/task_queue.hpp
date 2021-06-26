87fb42e434c2859f9889739f65b8d20b3ce73d07
industriousonesoft <industriousonesoft@gmail.com>
2021-06-26 17:34:22 +0800

    Added Task Queue

---
+++ include/base/task_queue.hpp
@@ -0,0 +1,32 @@
+#ifndef _BASE_TASK_QUEUE_H_
+#define _BASE_TASK_QUEUE_H_
+
+#include "common/defines.hpp"
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/io_context_strand.hpp>
+#include <boost/thread/thread.hpp>
+
+namespace naivertc {
+
+class RTC_CPP_EXPORT TaskQueue {
+public:
+    TaskQueue();
+    ~TaskQueue();
+
+    template<typename Function>
+    void Post(Function && f) const;
+
+    template<typename Function>
+    void Dispatch(Function && f) const;
+
+private:
+    boost::asio::io_context ioc_;
+    boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard_;
+    boost::asio::io_context::strand strand_;
+    std::unique_ptr<boost::thread> ioc_thread_;
+};
+
+}
+
+#endif
---
+++ src/base/task_queue.cpp
@@ -0,0 +1,30 @@
+#include "base/task_queue.hpp"
+
+#include <boost/asio.hpp>
+
+namespace naivertc {
+
+TaskQueue::TaskQueue() 
+    : work_guard_(boost::asio::make_work_guard(ioc_)),
+      strand_(ioc_) {
+    ioc_thread_.reset(new boost::thread(boost::bind(&boost::asio::io_context::run, &ioc_)));
+    ioc_thread_->detach();
+}
+
+TaskQueue::~TaskQueue() {
+    ioc_.stop();
+    // ioc_thread_ will exist after ioc stoped.
+    ioc_thread_.reset();
+}
+
+template<typename Function>
+void TaskQueue::Post(Function && f) const {
+    boost::asio::post(strand_, f);
+}
+
+template<typename Function>
+void TaskQueue::Dispatch(Function && f) const {
+    boost::asio::dispatch(strand_, f);
+}
+
+}
