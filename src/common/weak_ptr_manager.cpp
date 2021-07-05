786792ca94e64d7ce12e5f2eb8e7b3145e791dc1
industriousonesoft <industriousonesoft@gmail.com>
2021-07-05 17:56:37 +0800

    Check pointer under the lock

--- src/common/weak_ptr_manager.cpp
+++ src/common/weak_ptr_manager.cpp
@@ -10,21 +10,22 @@ WeakPtrManager::~WeakPtrManager() {
 }
 
 void WeakPtrManager::Register(void* ptr) {
-    if (ptr == nullptr) return;
     // 排他锁：一读一写
     std::unique_lock lock(mutex_);
+    if (ptr == nullptr) return;
     ptr_set_.insert(ptr);
 }
 
 void WeakPtrManager::Deregister(void* ptr) {
-    if (ptr == nullptr) return;
     std::unique_lock lock(mutex_);
+    if (ptr == nullptr) return;
     ptr_set_.erase(ptr);
 }
 
 std::optional<std::shared_lock<std::shared_mutex>> WeakPtrManager::TryLock(void* ptr) {
     // 共享锁: 多读一写
     std::shared_lock<std::shared_mutex> lock(mutex_);
+    if (!ptr) return std::nullopt;
     return ptr_set_.find(ptr) != ptr_set_.end() ? std::make_optional(std::move(lock)) : std::nullopt;
 }
 
