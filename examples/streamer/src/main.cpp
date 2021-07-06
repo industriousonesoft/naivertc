de59baea5a7cbddbd24d20498cc2055bf6c29845
industriousonesoft <industriousonesoft@gmail.com>
2021-07-06 10:34:52 +0800

    Code Optimized

--- examples/streamer/src/main.cpp
+++ examples/streamer/src/main.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+// naivertc
+#include <common/logger.hpp>
 
 // boost
 #include <boost/asio.hpp>
@@ -8,25 +9,27 @@
 
 int main(int argc, const char* argv[]) {
 
+    // Logger
+    naivertc::logging::InitLogger(naivertc::logging::Level::VERBOSE);
+
     boost::asio::io_context ioc;
     boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard(ioc.get_executor());
 
     boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
     signals.async_wait([&](const boost::system::error_code&, int) {
         ioc.stop();
-        std::cout << "main ioc exit" << std::endl;
+        PLOG_DEBUG << "main ioc exit";
     });
 
-    std::cout << "main start." << std::endl;
-    
-    std::shared_ptr<Client> client = Client::Create(ioc);
+    PLOG_DEBUG << "main start.";
 
+    std::shared_ptr<Client> client = Client::Create(ioc);
     client->Start();
 
     ioc.run();
 
     client->Stop();
-    std::cout << "main exit." << std::endl;
+    PLOG_DEBUG << "main exit.";
 
     return 0;
 }
