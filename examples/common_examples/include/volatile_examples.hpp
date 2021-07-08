8dab1c58d0175bb469b8777b4091c421e9f0e5be
industriousonesoft <industriousonesoft@gmail.com>
2021-07-08 11:08:35 +0800

    Added Volatile Example, but failed

---
+++ examples/common_examples/include/volatile_examples.hpp
@@ -0,0 +1,12 @@
+#ifndef _VOLATILE_EXAMPLES_H_
+#define _VOLATILE_EXAMPLES_H_
+
+namespace volatile_tests {
+
+void WithoutVolatile();
+void WithVolatile();
+    
+} // namespace volatile
+
+
+#endif
--- examples/common_examples/src/main.cpp
+++ examples/common_examples/src/main.cpp
@@ -1,32 +1,20 @@
 #include "task_queue_examples.hpp"
+#include "volatile_examples.hpp"
 
 // naivertc
 #include <common/logger.hpp>
-
-// boost
-#include <boost/asio.hpp>
-#include <boost/asio/io_context.hpp>
 
 int main(int argc, const char* argv[]) {
 
     // Logger
     naivertc::logging::InitLogger(naivertc::logging::Level::VERBOSE);
 
-    boost::asio::io_context ioc;
-    boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard(ioc.get_executor());
-
-    boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
-    signals.async_wait([&](const boost::system::error_code&, int) {
-        ioc.stop();
-        PLOG_DEBUG << "main ioc exit";
-    });
-
     // TaskQueue
     // taskqueue::DelayPostTest();
-    taskqueue::PostTest();
+    // taskqueue::PostTest();
 
-
-    ioc.run();
+    // Volatile
+    // volatile_tests::WithoutVolatile();
 
     return 0;
 }
