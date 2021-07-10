85de28363585fbdc0190f58e4dffa97c2afe23d5
industriousonesoft <industriousonesoft@gmail.com>
2021-07-10 19:39:20 +0800

    Try to init DTLS transport

--- include/pc/peer_connection.hpp
+++ include/pc/peer_connection.hpp
@@ -102,6 +102,7 @@ protected:
 
 private:
     void InitIceTransport();
+    void InitDtlsTransport();
     void InitSctpTransport();
 
     bool UpdateConnectionState(ConnectionState state);
---
+++ src/pc/peer_connection_dtls_transport_delegate.cpp
@@ -0,0 +1,9 @@
+#include "pc/peer_connection.hpp"
+
+namespace naivertc {
+
+void PeerConnection::InitDtlsTransport() {
+
+}
+
+} // namespace naivertc
