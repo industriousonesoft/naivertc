62ca7cad6218bc3cd2d211dcead606f09cb6bec0
industriousonesoft <industriousonesoft@gmail.com>
2021-07-10 17:55:00 +0800

    New candidate unit tests

--- src/pc/sdp/candidate_unittest.cpp
+++ src/pc/sdp/candidate_unittest.cpp
@@ -25,6 +25,21 @@ TEST(CandidateTest, CreateFromSDPLine) {
 
 }
 
+TEST(CandidateTest, BuildFromCandidateSDP) {
+    const std::string sdp = "candidate:1 1 UDP 9654321 212.223.223.223 12345 typ srflx raddr 10.216.33.9 rport 54321";
+
+    naivertc::Candidate candidate(sdp);
+
+    EXPECT_EQ(candidate.foundation(), "1");
+    EXPECT_EQ(candidate.component_id(), 1);
+    EXPECT_EQ(candidate.transport_type(), Candidate::TransportType::UDP);
+    EXPECT_EQ(candidate.priority(), 9654321);
+    EXPECT_EQ(candidate.host_name(), "212.223.223.223");
+    EXPECT_EQ(candidate.service(), "12345");
+    EXPECT_EQ(candidate.type(), Candidate::Type::SERVER_REFLEXIVE);
+    EXPECT_EQ(candidate.isResolved(), false);
+}
+
 TEST(CandidateTest, BuildSDPLine) {
     const std::string sdp = "a=candidate:1 1 UDP 9654321 212.223.223.223 12345 typ srflx raddr 10.216.33.9 rport 54321";
 
