0787e0a58ccb335801026669743b26c5dc058bb0
industriousonesoft <industriousonesoft@gmail.com>
2021-07-14 11:47:44 +0800

    Added MediaTrack unittest

--- src/pc/media/media_track.cpp
+++ src/pc/media/media_track.cpp
@@ -9,16 +9,16 @@ MediaTrack::Config::Config::Config(std::string _mid,
                                     const std::vector<int> _payload_types, 
                                     uint32_t _ssrc, 
                                     std::optional<std::string> _cname, 
-                                    std::optional<std::string> _msidt,
+                                    std::optional<std::string> _msid,
                                     std::optional<std::string> _track_id) 
     : mid(std::move(_mid)),
     kind(_kind),
     codec(_codec),
     payload_types(std::move(_payload_types)),
     ssrc(_ssrc),
-    cname(_cname),
-    msid(_mid),
-    track_id(_track_id) {}
+    cname(std::move(_cname)),
+    msid(std::move(_msid)),
+    track_id(std::move(_track_id)) {}
 
 // Media track
 MediaTrack::MediaTrack(const sdp::Media& description) 
