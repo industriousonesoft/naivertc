5f90129ed4d73f57cd999eaf9628ede1cb61c585
industriousonesoft <industriousonesoft@gmail.com>
2021-07-14 16:46:02 +0800

    Try streamer example

---
+++ examples/streamer/src/client_signaling_delegate.cpp
@@ -0,0 +1,64 @@
+#include "client.hpp"
+
+#include <iostream>
+
+void Client::OnIceServers(std::vector<IceServer> ice_servers) {
+    std::cout << __FUNCTION__ << std::endl;
+    ioc_.post(strand_.wrap([this, ice_servers = std::move(ice_servers)](){
+
+        RtcConfiguration rtc_config;
+        rtc_config.ice_servers = std::move(ice_servers);
+
+        this->CreatePeerConnection(std::move(rtc_config));
+
+    }));
+}
+
+void Client::OnConnected(bool is_initiator) {
+    std::cout << __FUNCTION__ << " is_initiator: " << is_initiator << std::endl;
+    ioc_.post(strand_.wrap([this, is_initiator](){
+        // If we are not initiator, we act as a offer
+        if (!is_initiator) {
+            this->peer_conn_->CreateOffer([this](const sdp::SessionDescription& local_sdp){
+                std::cout << "Did create local offer sdp " << std::endl;
+                this->SendLocalSDP(std::move(local_sdp), true);
+            }, [](const std::exception& exp){
+                std::cout << "Failed to create offer: " << exp.what() << std::endl;
+            });
+        }
+    }));
+}
+
+void Client::OnClosed(boost::system::error_code ec) {
+    std::cout << __FUNCTION__ << ": Signaling channel did close" <<std::endl;
+}
+
+void Client::OnRemoteSDP(const std::string sdp, bool is_offer) {
+    ioc_.post(strand_.wrap([this, remote_sdp = std::move(sdp), is_offer](){
+        if (is_offer) {
+            this->peer_conn_->SetOffer(std::move(remote_sdp), [this](){
+                std::cout << "Did set remote offer " << std::endl;
+                this->peer_conn_->CreateAnswer([this](const sdp::SessionDescription& local_sdp){
+                    std::cout << "Did create local answer sdp" << std::endl;
+                    this->SendLocalSDP(std::move(local_sdp), false);
+                }, [](const std::exception& exp){
+                    std::cout << "Failed to create answer: " << exp.what() << std::endl;
+                });
+            }, [](const std::exception& exp){
+                std::cout << "Failed to set remote offer: " << exp.what() << std::endl;
+            });
+        }else {
+            this->peer_conn_->SetAnswer(std::move(remote_sdp), [](){
+                std::cout << "Did set remote answer " << std::endl;
+            }, [](const std::exception& exp){
+                std::cout << "Failed to set remote answer: " << exp.what() << std::endl;
+            });
+        }
+    }));
+}
+
+void Client::OnRemoteCandidate(const std::string sdp_mid, const int sdp_mlineindex, const std::string candidate) {
+    ioc_.post(strand_.wrap([this, mid = std::move(sdp_mid), sdp = std::move(candidate)](){
+        this->peer_conn_->AddRemoteCandidate(std::move(mid), std::move(sdp));
+    }));
+}
