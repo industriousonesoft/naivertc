c9dec76146f32c3fcb12535782f3c55c116f4159
industriousonesoft <industriousonesoft@gmail.com>
2021-07-20 15:47:39 +0800

    Parameter corrected

--- src/rtc/sdp/sdp_media_entry_unittest.cpp
+++ src/rtc/sdp/sdp_media_entry_unittest.cpp
@@ -15,8 +15,7 @@ TEST(SDPEntryTest, CreateApplicationEntry) {
     sdp::Application app("data");
 
     EXPECT_EQ(app.mid(), "data");
-    EXPECT_EQ(app.type(), sdp::MeidaEntry::Type::APPLICATION);
-    EXPECT_EQ(app.direction(), sdp::Direction::SEND_RECV);
+    EXPECT_EQ(app.type(), sdp::MediaEntry::Type::APPLICATION);
 }
 
 // Video entry
@@ -24,7 +23,7 @@ TEST(SDPEntryTest, CreateVideoEntry) {
     sdp::Video video("video", sdp::Direction::SEND_ONLY);
 
     EXPECT_EQ(video.mid(), "video");
-    EXPECT_EQ(video.type(), sdp::MeidaEntry::Type::VIDEO);
+    EXPECT_EQ(video.type(), sdp::MediaEntry::Type::VIDEO);
     EXPECT_EQ(video.direction(), sdp::Direction::SEND_ONLY);
 }
 
@@ -33,9 +32,9 @@ TEST(SDPEntryTest, CreateAudioEntry) {
     sdp::Audio audio("audio", sdp::Direction::RECV_ONLY);
 
     EXPECT_EQ(audio.mid(), "audio");
-    EXPECT_EQ(audio.type(), sdp::MeidaEntry::Type::AUDIO);
+    EXPECT_EQ(audio.type(), sdp::MediaEntry::Type::AUDIO);
     EXPECT_EQ(audio.direction(), sdp::Direction::RECV_ONLY);
 }
 
-}
-}
+} // namespace test
+} // namespace naivertc
