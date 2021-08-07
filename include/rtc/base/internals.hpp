6c7113c1d08dd91147b050ac6c33da4f15296032
industriousonesoft <industriousonesoft@gmail.com>
2021-08-07 14:55:10 +0800

    Corrected const variables format

--- include/rtc/base/internals.hpp
+++ include/rtc/base/internals.hpp
@@ -6,13 +6,13 @@
 
 namespace naivertc {
 
-const size_t DEFAULT_SCTP_PORT = 5000;
-const size_t DEFAULT_LOCAL_MAX_MESSAGE_SIZE = 256 * 1024;
+const size_t kDefaultSctpPort = 5000;
+const size_t kDefaultLocalMaxMessageSize = 256 * 1024;
 // IPv6 minimum guaranteed MTU
-const size_t DEFAULT_MTU_SIZE = 1280;
+const size_t kDefaultMtuSize = 1280;
 
 using StreamId = uint16_t;
-const StreamId STREAM_ID_MAX_VALUE = 65535;
+const StreamId kStreamIdMaxValue = 65535;
 
 }
 
