d4df5767bf1f672df1bba5a3a71e836d937f24f2
industriousonesoft <industriousonesoft@gmail.com>
2021-08-09 19:01:50 +0800

    Added TimeDelta class and unittest

---
+++ include/rtc/base/time_delta.hpp
@@ -0,0 +1,80 @@
+#ifndef _RTC_BASE_TIME_DELTA_H_
+#define _RTC_BASE_TIME_DELTA_H_
+
+#include "base/defines.hpp"
+#include "rtc/base/unit_relative.hpp"
+
+#include <type_traits>
+
+namespace naivertc {
+
+// TimeDelta represents the difference between two timestamps. Commonly this can be 
+// a duration. However since two Timestamps are not guaranteed to have the same epoch
+// (they might come from different computers, make exact synchronisation infeasible.),
+// the duration covered by a TimeDelta can be undefined. To simplify usage, it can be 
+// constructed and converted to different units, specifically seconds(s), milliseconds(ms)
+// microseconds (us).
+class RTC_CPP_EXPORT TimeDelta : public RelativeUnit<TimeDelta> {
+public:
+    template <typename T>
+    static constexpr TimeDelta Seconds(T value) {
+        static_assert(std::is_arithmetic<T>::value, "");
+        return FromFraction(1'000'000, value);
+    }
+
+    template <typename T>
+    static constexpr TimeDelta Millis(T value) {
+        static_assert(std::is_arithmetic<T>::value, "");
+        return FromFraction(1'000, value);
+    }
+
+    template <typename T>
+    static constexpr TimeDelta Micros(T value) {
+        static_assert(std::is_arithmetic<T>::value, "");
+        return FromValue(value);
+    }
+
+    TimeDelta() = delete;
+
+    template <typename T = int64_t>
+    constexpr T seconds() const {
+        return ToFraction<1000000, T>();
+    }
+    template <typename T = int64_t>
+    constexpr T ms() const {
+        return ToFraction<1000, T>();
+    }
+    template <typename T = int64_t>
+    constexpr T us() const {
+        return ToValue<T>();
+    }
+    template <typename T = int64_t>
+    constexpr T ns() const {
+        return ToMultiple<1000, T>();
+    }
+
+    constexpr int64_t seconds_or(int64_t fallback_value) const {
+        return ToFractionOr<1000000>(fallback_value);
+    }
+    constexpr int64_t ms_or(int64_t fallback_value) const {
+        return ToFractionOr<1000>(fallback_value);
+    }
+    constexpr int64_t us_or(int64_t fallback_value) const {
+        return ToValueOr(fallback_value);
+    }
+
+    constexpr TimeDelta Abs() const {
+        return us() < 0 ? TimeDelta::Micros(-us()) : *this;
+    }
+
+private:
+    friend class UnitBase<TimeDelta>;
+    using RelativeUnit::RelativeUnit;
+    static constexpr bool one_sided = false;
+
+};
+    
+} // namespace naivertc
+
+
+#endif
---
+++ src/rtc/base/time_delta_unittest.cpp
@@ -0,0 +1,106 @@
+#include "rtc/base/time_delta.hpp"
+
+#include <gtest/gtest.h>
+
+namespace naivertc {
+namespace test {
+
+TEST(TimeDeltaBaseTest, ConstExpr) {
+    constexpr int64_t kValue = -12345;
+    constexpr TimeDelta kTestUnitZero = TimeDelta::Zero();
+    constexpr TimeDelta kTestUnitMaxValue = TimeDelta::MaxValue();
+    constexpr TimeDelta kTestUnitMinValue = TimeDelta::MinValue();
+
+    static_assert(kTestUnitZero.IsZero(), "");
+    static_assert(kTestUnitMaxValue.IsMax(), "");
+    static_assert(kTestUnitMinValue.IsMin(), "");
+    static_assert(kTestUnitMaxValue.ms_or(-1) == -1, "");
+
+    static_assert(kTestUnitMaxValue > kTestUnitZero, "");
+
+    constexpr TimeDelta kTimeDeltaSeconds = TimeDelta::Seconds(kValue);
+    constexpr TimeDelta kTimeDeltaMs = TimeDelta::Millis(kValue);
+    constexpr TimeDelta kTimeDeltaUs = TimeDelta::Micros(kValue);
+
+    static_assert(kTimeDeltaSeconds.seconds_or(0) == kValue, "");
+    static_assert(kTimeDeltaMs.ms_or(0) == kValue, "");
+    static_assert(kTimeDeltaUs.us_or(0) == kValue, "");
+}
+
+TEST(TimeDeltaBaseTest, GetBackSameValues) {
+    const int64_t kValue = 499;
+    for (int sign = -1; sign <= 1; ++sign) {
+        int64_t value = kValue * sign;
+        EXPECT_EQ(TimeDelta::Millis(value).ms(), value);
+        EXPECT_EQ(TimeDelta::Micros(value).us(), value);
+        EXPECT_EQ(TimeDelta::Seconds(value).seconds(), value);
+        EXPECT_EQ(TimeDelta::Seconds(value).seconds(), value);
+    }
+    EXPECT_EQ(TimeDelta::Zero().us(), 0);
+}
+
+TEST(TimeDeltaBaseTest, GetDifferentPrefix) {
+    const int64_t kValue = 3000000;
+    EXPECT_EQ(TimeDelta::Micros(kValue).seconds(), kValue / 1000000);
+    EXPECT_EQ(TimeDelta::Millis(kValue).seconds(), kValue / 1000);
+    EXPECT_EQ(TimeDelta::Micros(kValue).ms(), kValue / 1000);
+
+    EXPECT_EQ(TimeDelta::Millis(kValue).us(), kValue * 1000);
+    EXPECT_EQ(TimeDelta::Seconds(kValue).ms(), kValue * 1000);
+    EXPECT_EQ(TimeDelta::Seconds(kValue).us(), kValue * 1000000);
+}
+
+TEST(TimeDeltaBaseTest, ConvertsToAndFromDouble) {
+    const int64_t kMicros = 17017;
+    const double kNanosDouble = kMicros * 1e3;
+    const double kMicrosDouble = kMicros;
+    const double kMillisDouble = kMicros * 1e-3;
+    const double kSecondsDouble = kMillisDouble * 1e-3;
+
+    EXPECT_EQ(TimeDelta::Micros(kMicros).seconds<double>(), kSecondsDouble);
+    EXPECT_EQ(TimeDelta::Seconds(kSecondsDouble).us(), kMicros);
+
+    EXPECT_EQ(TimeDelta::Micros(kMicros).ms<double>(), kMillisDouble);
+    EXPECT_EQ(TimeDelta::Millis(kMillisDouble).us(), kMicros);
+
+    EXPECT_EQ(TimeDelta::Micros(kMicros).us<double>(), kMicrosDouble);
+    EXPECT_EQ(TimeDelta::Micros(kMicrosDouble).us(), kMicros);
+
+    EXPECT_NEAR(TimeDelta::Micros(kMicros).ns<double>(), kNanosDouble, 1);
+
+    const double kMaxValue = std::numeric_limits<double>::infinity();
+    const double kMinValue = -kMaxValue;
+
+    EXPECT_EQ(TimeDelta::MaxValue().seconds<double>(), kMaxValue);
+    EXPECT_EQ(TimeDelta::MinValue().seconds<double>(), kMinValue);
+    EXPECT_EQ(TimeDelta::MaxValue().ms<double>(), kMaxValue);
+    EXPECT_EQ(TimeDelta::MinValue().ms<double>(), kMinValue);
+    EXPECT_EQ(TimeDelta::MaxValue().us<double>(), kMaxValue);
+    EXPECT_EQ(TimeDelta::MinValue().us<double>(), kMinValue);
+    EXPECT_EQ(TimeDelta::MaxValue().ns<double>(), kMaxValue);
+    EXPECT_EQ(TimeDelta::MinValue().ns<double>(), kMinValue);
+
+    EXPECT_TRUE(TimeDelta::Seconds(kMaxValue).IsMax());
+    EXPECT_TRUE(TimeDelta::Seconds(kMinValue).IsMin());
+    EXPECT_TRUE(TimeDelta::Millis(kMaxValue).IsMax());
+    EXPECT_TRUE(TimeDelta::Millis(kMinValue).IsMin());
+    EXPECT_TRUE(TimeDelta::Micros(kMaxValue).IsMax());
+    EXPECT_TRUE(TimeDelta::Micros(kMinValue).IsMin());
+}
+
+TEST(TimeDeltaBaseTest, InfinityOperations) {
+  const int64_t kValue = 267;
+  const TimeDelta finite = TimeDelta::Millis(kValue);
+  EXPECT_TRUE((TimeDelta::MaxValue() + finite).IsMax());
+  EXPECT_TRUE((TimeDelta::MaxValue() - finite).IsMax());
+  EXPECT_TRUE((finite + TimeDelta::MaxValue()).IsMax());
+  EXPECT_TRUE((finite - TimeDelta::MinValue()).IsMax());
+
+  EXPECT_TRUE((TimeDelta::MinValue() + finite).IsMin());
+  EXPECT_TRUE((TimeDelta::MinValue() - finite).IsMin());
+  EXPECT_TRUE((finite + TimeDelta::MinValue()).IsMin());
+  EXPECT_TRUE((finite - TimeDelta::MaxValue()).IsMin());
+}
+    
+} // namespace test    
+} // namespace naivertc
