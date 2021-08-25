05fe3a89efbff27420c90032b43390ad3cfcec5b
industriousonesoft <industriousonesoft@gmail.com>
2021-08-25 12:07:28 +0800

    ArrayView optimized

--- include/common/array_view.hpp
+++ include/common/array_view.hpp
@@ -4,13 +4,34 @@
 #include "base/defines.hpp"
 
 #include <cstdint>
+#include <vector>
+#include <type_traits>
 
 namespace naivertc {
 
 template<typename T>
 class RTC_CPP_EXPORT ArrayView {
 public:
+    using value_type = T;
+    using const_iterator = const T*;
+
     ArrayView(T* ptr, size_t size) noexcept : ptr_(ptr), size_(size) {}
+  
+    template <typename U, size_t N>
+    ArrayView(U (&buffer)[N]) noexcept : ArrayView(buffer, N) {}
+
+    // ArrayView<T> to ArraryView<const T>
+    // std::vector<T> to ArraryView<const T> or ArraryView<T>
+    template <
+        typename U,
+        // Container has data and size
+        typename std::enable_if<
+            std::is_convertible<decltype(std::declval<U>().data()), T*>::value &&
+            std::is_convertible<decltype(std::declval<U>().size()), std::size_t>::value
+        >::type* = nullptr
+    >
+    // FIXME: 通过右值引用实现T to const T的转换？
+    ArrayView(const U& u) noexcept : ArrayView(u.data(), u.size()) {}
 
     T& operator[](int i) noexcept { return ptr_[i]; }
     T const& operator[](int i) const noexcept { return  ptr_[i]; }
