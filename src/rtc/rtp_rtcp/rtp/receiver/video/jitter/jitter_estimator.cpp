e6fe667b547f0493187095fc2c80c9fe60a43c12
industriousonesoft <industriousonesoft@gmail.com>
2021-11-01 19:14:58 +0800

    Try to add JitterEstimator

---
+++ src/rtc/rtp_rtcp/rtp/receiver/video/jitter/jitter_estimator.cpp
@@ -0,0 +1,11 @@
+#include "rtc/rtp_rtcp/rtp/receiver/video/jitter/jitter_estimator.hpp"
+
+namespace naivertc {
+namespace rtc {
+namespace video {
+namespace jitter {
+    
+} // namespace jitter
+} // namespace video
+} // namespace rtc
+} // namespace naivert 
---
+++ src/rtc/rtp_rtcp/rtp/receiver/video/jitter/jitter_estimator.hpp
@@ -0,0 +1,21 @@
+#ifndef _RTC_RTP_RTCP_RTP_FEC_RECEIVER_VIDEO_JITTER_JITTER_ESTIMATOR_H_
+#define _RTC_RTP_RTCP_RTP_FEC_RECEIVER_VIDEO_JITTER_JITTER_ESTIMATOR_H_
+
+#include "base/defines.hpp"
+
+namespace naivertc {
+namespace rtc {
+namespace video {
+namespace jitter {
+
+class RTC_CPP_EXPORT JitterEstimator {
+
+};
+    
+} // namespace jitter
+} // namespace video
+} // namespace rtc
+} // namespace naivert 
+
+
+#endif
