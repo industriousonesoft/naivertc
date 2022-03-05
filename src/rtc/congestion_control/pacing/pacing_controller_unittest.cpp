b94747a953a923838fa27f5f2208ef9f00464516
industriousonesoft <industriousonesoft@gmail.com>
2022-03-05 17:40:08 +0800

    Try to test ProbeController

---
+++ src/rtc/congestion_control/pacing/pacing_controller_unittest.cpp
@@ -0,0 +1,14 @@
+#include "rtc/congestion_control/pacing/pacing_controller.hpp"
+
+#include <gtest/gtest.h>
+
+#define ENABLE_UNIT_TESTS 1
+#include "testing/defines.hpp"
+
+namespace naivertc {
+namespace test {
+
+
+    
+} // namespace test    
+} // namespace naivertc
