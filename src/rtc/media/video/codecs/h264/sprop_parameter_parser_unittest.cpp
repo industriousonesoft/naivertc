1448124ee369fc95dad167925715bc4c80e3552b
industriousonesoft <industriousonesoft@gmail.com>
2022-03-05 18:06:21 +0800

    Code optimization

--- src/rtc/media/video/codecs/h264/sprop_parameter_parser_unittest.cpp
+++ src/rtc/media/video/codecs/h264/sprop_parameter_parser_unittest.cpp
@@ -18,9 +18,9 @@ public:
 MY_TEST_F(SpropParameterParserTest, Base64DecodeSprop) {
     // Example sprop string from https://tools.ietf.org/html/rfc3984 .
     EXPECT_TRUE(h264_sprop.Parse("Z0IACpZTBYmI,aMljiA=="));
-    static const std::vector<uint8_t> raw_sps{0x67, 0x42, 0x00, 0x0A, 0x96,
+    static const std::vector<uint8_t> raw_sps = {0x67, 0x42, 0x00, 0x0A, 0x96,
                                               0x53, 0x05, 0x89, 0x88};
-    static const std::vector<uint8_t> raw_pps{0x68, 0xC9, 0x63, 0x88};
+    static const std::vector<uint8_t> raw_pps = {0x68, 0xC9, 0x63, 0x88};
     EXPECT_EQ(raw_sps, h264_sprop.sps_nalu());
     EXPECT_EQ(raw_pps, h264_sprop.pps_nalu());
 }
