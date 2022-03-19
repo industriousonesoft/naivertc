2cbcfe268ac65898b4eacb09a75dbd81f3c1500b
industriousonesoft <industriousonesoft@gmail.com>
2022-03-19 12:54:41 +0800

    BugFixed: don't rest repeating task in Clear

--- src/rtc/call/rtp_send_controller.cpp
+++ src/rtc/call/rtp_send_controller.cpp
@@ -3,6 +3,8 @@
 #include "rtc/rtp_rtcp/rtcp/packets/transport_feedback.hpp"
 #include "rtc/congestion_control/send_side/goog_cc/goog_cc_network_controller.hpp"
 
+#include <plog/Log.h>
+
 namespace naivertc {
 namespace {
 
@@ -42,7 +44,6 @@ void RtpSendController::Clear() {
         if (controller_task_ && controller_task_->Running()) {
             controller_task_->Stop();
         }
-        controller_task_.reset();
     });
 }
 
