b32c2ca66564a5557f1990dda5eb5c1507f26301
industriousonesoft <industriousonesoft@gmail.com>
2022-03-24 15:16:34 +0800

    TaskQueueBoost: private implementation

--- src/rtc/base/task_utils/task_queue_impl_boost.hpp
+++ src/rtc/base/task_utils/task_queue_impl_boost.hpp
@@ -14,51 +14,8 @@
 
 namespace naivertc {
 
-class TaskQueueImplBoost final : public TaskQueueImpl {
-public:
-    static std::unique_ptr<TaskQueueImpl, TaskQueueImpl::Deleter> Create(std::string name);
-public:
-    void Delete() override;
-    void Post(std::unique_ptr<QueuedTask> task) override;
-    void PostDelayed(TimeDelta delay, std::unique_ptr<QueuedTask> task) override;
+std::unique_ptr<TaskQueueImpl, TaskQueueImpl::Deleter> CreateTaskQueueBoost(std::string_view name);
 
-private:
-    // Users of the TaskQueue should call Create instead of 
-    // directly create this instance.
-    TaskQueueImplBoost(std::string name);
-    // Users of the TaskQueue should call Delete instead of 
-    // directly deleting this instance.
-    ~TaskQueueImplBoost() override;
-
-    struct ScopedQueuedTask;
-    void ScheduleTaskAfter(TimeDelta delay, ScopedQueuedTask&& scoped_task);
-
-private:
-    // ScopedQueuedTask
-    struct ScopedQueuedTask {
-    public: 
-        ScopedQueuedTask(std::unique_ptr<QueuedTask> queued_task) 
-            : queued_task_(std::move(queued_task)) {}
-
-        void operator()() {
-            if (queued_task_) {
-                queued_task_->Run();
-            }
-        }   
-
-    private:
-        std::unique_ptr<QueuedTask> queued_task_;
-    };
-
-private:
-    boost::asio::io_context ioc_;
-    boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard_;
-    boost::asio::io_context::strand strand_;
-    std::unique_ptr<boost::thread> ioc_thread_;
-
-    std::list<boost::asio::deadline_timer*> pending_timers_;
-};
-    
 } // namespace naivertc
 
 
